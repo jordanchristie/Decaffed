5bbcbcb86fb98d96ac934ff2cb512eb85e431be4
Jordan Christie <jordanchristie116@gmail.com>
2018-11-14 22:47:05 -0500

    Created CoffeeShopModal.js

--- client/src/components/CoffeeMap.js
+++ client/src/components/CoffeeMap.js
@@ -1,49 +1,76 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { GoogleApiWrapper, Map } from 'google-maps-react';
+import { GoogleApiWrapper, Map, Marker, InfoWindow } from 'google-maps-react';
 
-import CoffeeShop from './CoffeeShop';
+import CoffeeShopModal from './CoffeeShopModal';
 import { fetchCoffeeShops } from '../actions';
 import keys from '../keys.json';
 import './CoffeeMap.css';
 
 class CoffeeMap extends Component {
-
-    // componentDidMount() {
-    //     this.loadCoffeeShops();
-    // }
-    
-    loadCoffeeShops = () => {
-        this.props.fetchCoffeeShops(this.props.coordinates, this.props.google)
+    constructor(props) {
+        super();
+        this.state = {
+            activeMarker: null,
+            selectedPlace: {},
+            infoWindowOpen: false
+        }
     }
 
-    renderMarkers = () => {
-        const {coffeeShops} = this.props
-        coffeeShops.map((shop, i) => {
-            return <CoffeeShop
-                        key={shop.id}
-                        coffeeShop={shop}
-                    />
+    
+    selectMarker = (props,marker) => {
+        this.setState({
+            activeMarker: marker,
+            selectedPlace: props.shop,
+            infoWindowOpen: true
         })
     }
+
+    renderModal = () => {
+        console.log('clicked')
+        return <CoffeeShopModal />
+    }
+
 
     render() {
         const style = {
             width: '100vw',
             height: '100vh'
         }
+        const {selectedPlace} = this.state;
         return (
             <div id="map">
                 <Map
                     item
                     xs={14}
                     style={style}
-                    zoom={14}
+                    zoom={17}
                     google={this.props.google}
                     initialCenter={this.props.coordinates}
-                    onReady={this.renderMarkers()}
                 >
-                {this.renderMarkers}
+                {this.props.coffeeShops.map((shop, i) => {
+                return <Marker
+                        key={i}
+                        shop={shop} 
+                        title={shop.name}
+                        name={shop.name}
+                        position={{lat: shop.coordinates.latitude, lng: shop.coordinates.longitude}}
+                        animation={this.props.google.maps.Animation.DROP}
+                        onClick={this.selectMarker}/>
+                })}
+                { 
+                    this.state.activeMarker && 
+                        <InfoWindow
+                        marker={this.state.activeMarker}
+                        visible={this.state.infoWindowOpen}
+                        >
+                        <img src={selectedPlace.image_url} alt="place" class="shop-img"/>
+                        <h4 onClick={this.renderModal}>{selectedPlace.name}</h4>
+                       
+                        </InfoWindow>
+
+                }
+                
                 </Map>
             </div>   
         )
